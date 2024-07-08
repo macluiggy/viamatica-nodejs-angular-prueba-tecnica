604f1132c6a255b42bcd7bccbee3246b7b4a74f9
macluiggy <luiggy.macias@imagineapps.co>
2024-07-08 14:43:57 -0500

    chore: Update welcome component to display user information

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StorageService } from '../services/storage/storage.service';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
-
+  user = this.storageService.getUser();
+  constructor(private storageService: StorageService) {
+    console.log(this.user);
+    
+  }
 }
