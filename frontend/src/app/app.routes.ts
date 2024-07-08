9bfe876203267feb04df048c290250865bcb28c8
macluiggy <luiggy.macias@imagineapps.co>
2024-07-08 17:42:47 -0500

    Refactor user maintenance component to handle bulk user creation from file and include download template button

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BoardUserComponent } from './board-user/board-user.component';
-import { authGuard } from './guards/auth.guard';
 import { MyLoginHistoryComponent } from './my-login-history/my-login-history.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,7 +10,6 @@ import { UserMaintenanceComponent } from './user-maintenance/user-maintenance.co
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'user', component: BoardUserComponent },
   { path: 'my-login-history', component: MyLoginHistoryComponent },
