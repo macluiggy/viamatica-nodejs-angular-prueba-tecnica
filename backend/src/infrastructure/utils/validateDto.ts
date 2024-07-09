70dd32c2670645d9d0de8f78e91d91ac06e9c506
macluiggy <luiggy.macias@imagineapps.co>
2024-07-09 14:56:59 -0500

    Refactor validateDto.ts to remove commented out code and unused imports

--- backend/src/infrastructure/utils/validateDto.ts
+++ backend/src/infrastructure/utils/validateDto.ts
@@ -1,39 +1,5 @@
-// src/infrastructure/middleware/validationMiddleware.ts
 import { ClassConstructor, plainToInstance } from "class-transformer";
 import { validate, ValidationError } from "class-validator";
-import { Request, Response, NextFunction, RequestHandler } from "express";
-import HttpResponse from "../http/HttpResponse";
-import { StatusCodes } from "http-status-codes";
-
-// export function validationMiddleware<T>(
-//   type: ClassConstructor<T>
-// ): RequestHandler {
-//   return (req: Request, res: Response, next: NextFunction): void => {
-//     const dto = plainToInstance<T, T>(type, req.body);
-//     validate(dto, {
-//       whitelist: true,
-//       forbidNonWhitelisted: true,
-//     }).then((errors: ValidationError[]) => {
-//       if (errors.length > 0) {
-//         const message = errors
-//           .map((error: ValidationError) => Object.values(error.constraints!))
-//           .flat()
-//           .join(", ");
-//         const statusCode = StatusCodes.BAD_REQUEST;
-//         const responseBody = new HttpResponse({
-//           statusCode: statusCode,
-//           message,
-//           data: null,
-//           isOk: false,
-//         });
-//         res.status(statusCode).json(responseBody);
-//       } else {
-//         req.body = dto;
-//         next();
-//       }
-//     });
-//   };
-// }
 
 const validateDto = async <T>({
   type,
